df43814b26a607399f268db2a7ff6b468092ef5d
Dimash <Dimash>
2024-03-24 23:46:05 +0300

    Initial Commit

---
+++ kazist/kazistApp.swift
@@ -0,0 +1,17 @@
+//
+//  kazistApp.swift
+//  kazist
+//
+//  Created by Динмухамед Джилкайдаров on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct kazistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
